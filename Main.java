9cedc7f2ec79bcd3235057404f16475c38dbe71c
Eyano07 <pascaltshingila3535@gmail.com>
2024-11-28 20:56:49 +0100

    dish

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main{
     	public static void main(String[] args){
-        	System.out.println("Hello pascal Eyano!");
+        	System.out.println("Hello pascal DISASHI!");
         }
  }
