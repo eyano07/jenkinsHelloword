c1097c7dad6ec8cb0e3e82e6965b5800ed062846
eyano07 <143529968+eyano07@users.noreply.github.com>
2024-11-28 20:18:20 +0100

    Update public class Main public.java

--- public class Main public.java
+++ public class Main public.java
@@ -1,5 +1,5 @@
 public class Main{
     	public static void main(String[] args){
-        	System.out.println("Hello pascal !");
+        	System.out.println("Hello pascal Tshingila!");
         }
  }
